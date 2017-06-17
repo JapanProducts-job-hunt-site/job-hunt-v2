ac352d883f00c4670ec415ff206137056c5080ae
Itaru7 <ir172178@gmail.com>
2017-06-17 15:15:40 -0700

    Revert "test"
    This reverts commit 281415cb25fdb04d21522173ad83a2324b3eb574.

--- routes/back/user/index.js
+++ routes/back/user/index.js
@@ -7,7 +7,8 @@ var router = express.Router();
 var ctrlIndex = require("/index.js");
 var ctrlMyPage = require("/public/static/myPage.js");
 
-//index.htm　テシと
+//index.html
 router.post("/insert", ctrlIndex.register());
 router.get("/getData", ctrlIndex.login());
 
+testChange

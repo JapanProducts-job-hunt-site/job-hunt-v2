58b7279029c74a915dc7348a848d0cb49d7c7f20
Itaru7 <ir172178@gmail.com>
2017-06-16 21:50:19 -0700

    a

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by KishikawaItaru on 2017/06/14.
+ */
+var express = require('express');
+var router = express.Router();
+
+var ctrlIndex = require("/index.js");
+var ctrlMyPage = require("/public/static/myPage.js");
+
+//index.html
+router.post("/insert", ctrlIndex.register());
+router.get("/getData", ctrlIndex.login());
+
+testChange

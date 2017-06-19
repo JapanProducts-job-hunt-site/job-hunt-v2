dc9b1e5ef94fa64a3efc283c8a942c8ae2196d08
Itaru7 <ir172178@gmail.com>
2017-06-19 14:48:58 -0700

    localhost4000でindexが表示されるようになった

--- routes/back/user/index.js
+++ routes/back/user/index.js
@@ -8,5 +8,7 @@ var ctrlIndex = require("../../../controller/ctrlIndex");
 //var ctrlMyPage = require("/public/static/myPage");
 
 //index.html
-router.post("/insert", ctrlIndex.register());
-router.get("/getData", ctrlIndex.login());
+router.post("/insert", ctrlIndex.register);
+router.get("/getData", ctrlIndex.login);
+
+module.exports = router;

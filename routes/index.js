6480210140e5c6fd285866937637077c390bdd41
yk <yukikuro1993@gmail.com>
2017-06-25 16:32:48 -0700

    Add functionality to mypage to show username

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 // Get Homepage
-router.get('/', ensureAuthenticated, function(req, res){
-	res.render('index');
+router.get('/', ensureAuthenticated, function (req, res) {
+
+	// Rendering to mypage (index) with username and _id
+	console.log('[Login to mypage] username:' + req.user.name);
+	res.render('index', {
+		username: req.user.name,
+		db_id: req.user._id
+	});
 });
 
 // Prevent user from seeing dashboard page without logging in
-function ensureAuthenticated(req, res, next){
-	if(req.isAuthenticated()){
+function ensureAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
 		// if logged in keep going
 		return next();
 	} else {

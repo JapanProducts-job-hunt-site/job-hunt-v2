af5b65c7a0b1f49205731ee46de4abefffdc9918
YukiKuroshima <yukikuro1993@gmail.com>
2017-06-30 19:00:02 -0700

    Login working;

--- models/user.js
+++ models/user.js
@@ -43,12 +43,16 @@ module.exports.getUserByUsername = (username, callback) => {
   User.findOne(query, callback);
 }
 
+module.exports.getEmailById = (id, callback) => {
+  User.findOne(id, callback);
+}
+
 module.exports.comparePassword = (candidatePassword, hash, callback) => {
   // https://www.npmjs.com/package/bcrypt
 
   // Load hash from your password DB. 
   bcrypt.compare(candidatePassword, hash, function (err, isMatch) {
-    if(err) throw err;
+    if (err) throw err;
     callback(null, isMatch);
   });
 }
